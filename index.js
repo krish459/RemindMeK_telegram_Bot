8452cbcee04e05afb290047b24f8cac956bd4f0d
Krish <krishshah235@gmail.com>
2024-07-18 23:35:05 +0530

    Deploy start

--- index.js
+++ index.js
@@ -47,13 +47,13 @@ bot.on("message", async (msg) => {
 
 const cleanUpInterval = 24 * 60 * 60 * 1000; // Clean up once a day
 setInterval(() => {
-  console.log("Clean Up running");
+  console.log("Clean Up running!");
   deleteExpiredAlerts();
 }, cleanUpInterval);
 
 const interval = 60000; // Check every minute
 setInterval(() => {
-  console.log("Finding the alerts");
+  console.log("Finding the alerts!");
   checkAndSendAlerts(bot);
 }, interval);
 
